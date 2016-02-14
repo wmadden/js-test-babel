f64b5bcd1c126599603dab28e30555f48c40d8a0
Will Madden <w.a.madden@gmail.com>
2016-02-14 21:46:34 +0100

    Adjust `eslint` rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
-  "extends": "airbnb/base",
-  "parser": "babel-eslint",
-  "plugins": [
+  extends: "airbnb/base",
+  parser: "babel-eslint",
+  plugins: [
   ],
-  "rules": {
+  rules: {
     quotes: [2, "double"],
     "comma-dangle": [2, "always-multiline"],
+    "max-len": [2, 120],
+    "new-cap": 0,
   },
 };
